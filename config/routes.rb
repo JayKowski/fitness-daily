db9505930bba0422228fb7cc6879fb2f381107d3
jaymaina <jaymaina5@gmail.com>
2020-11-10 19:22:02 +0300

    added restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :reps
-  resources :stats
-  resources :workouts
-  resources :categories
-  resources :users
+  post 'v1/login' => 'v1/users#login'
+  get 'v1/auto_login' => 'v1/users#auto_login'
+  post 'v1/signup' => 'v1/users#create'
+  
+  namespace :v1 do
+    resources :reps
+    resources :stats
+    resources :workouts
+    resources :categories
+    resources :users
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
